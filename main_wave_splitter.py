c406bab28c1e7224a769d46e371e0acf5d7ff8a7
BaronVladziu <baronvladziu@gmail.com>
2019-02-08 14:16:00 +0100

    Period spliting done

--- main_wave_splitter.py
+++ main_wave_splitter.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from scipy.io import wavfile
+from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -41,33 +42,74 @@ min_voice_period = int(fs/max_voice_frequency)
 max_voice_period = int(fs/min_voice_frequency) + 1
 print("Voice period: <", min_voice_period, " - ", max_voice_period, ">")
 
+# Filtering
+b, a = signal.butter(10, max_voice_frequency/fs, "low")
+filtered_data = signal.filtfilt(b, a, data)
+
+# Calculating derivative
+dt = 1/fs
+der = np.zeros(len(data))
+der[0] = (filtered_data[1] - filtered_data[0])/dt
+for i in range(1, len(data) - 1):
+    der[i] = (filtered_data[i+1] - filtered_data[i-1])/(2*dt)
+der[len(data) - 1] = (filtered_data[len(data) - 1] - filtered_data[len(data) - 2])/dt
+
+# Finding local maximas
 zero_crossings = list()
-for i in range(0, len(data) - 2):
-    if data[i] < 0 and data[i+1] > 0:
+for i in range(0, len(data) - 1):
+    if der[i] > 0 and der[i+1] < 0:
         zero_crossings.append(i)
 
-for i in range(0, len(zero_crossings) - 1):
+# Removing unnecessary maximas
+global_threshold = 0.1
+i = 0
+while i < len(zero_crossings) - 1:
     # Calculate period
-    correlation = np.zeros(max_voice_period)
-    for j in range(min_voice_period, max_voice_period - 1):
-        for k in range(zero_crossings[i], zero_crossings[i] + max_voice_period - 1):
-            correlation[j - min_voice_period] += data[k]*data[k+j]
-    period = np.argmax(correlation)
-    # Remove unnecessary zeros
+    subdata_length = min(zero_crossings[i]+2*max_voice_period-1, len(data)-1) - zero_crossings[i]
+    window_length = subdata_length - max_voice_period
+    diff = np.zeros(max_voice_period + 1)
+    cum_diff = np.zeros(max_voice_period + 1)
+    diff[0] = 1
+    cum_diff[0] = 1
+    act_min_id = 0
+    act_min_val = 1
+    found_global_th = False
+    for j in range(1, max_voice_period+1):
+        d = filtered_data[zero_crossings[i] : zero_crossings[i]+window_length] - filtered_data[zero_crossings[i]+j : zero_crossings[i]+j+window_length]
+        diff[j] = np.sum(np.power(d, 2))
+        cum_diff[j] = diff[j] / np.mean(diff[0:j+1])
+        if found_global_th and cum_diff[j] > cum_diff[j-1]:
+            act_min_id = j-1
+            break
+        else:
+            if cum_diff[j] < global_threshold:
+                found_global_th = True
+            elif cum_diff[j] < act_min_val:
+                act_min_id = j
+                act_min_val = cum_diff[j]
+    period = act_min_id
+    # Remove unnecessary maximas
+    while i+2 < len(zero_crossings) and abs(zero_crossings[i+2] - zero_crossings[i] - period) < abs(zero_crossings[i+1] - zero_crossings[i] - period):
+        zero_crossings.pop(i+1)
+    i += 1
 
 
-
-
-
+# Printing
 zero_data = np.zeros(len(data))
 for zero in zero_crossings:
     zero_data[zero] = 30000
 
-length = 5000
-start = 50000
-plt.plot(data[start : start+length])
+length = 30000
+start = 30000
+plt.plot(filtered_data[start : start+length])
 plt.plot(zero_data[start : start+length])
-# plt.plot(peaks[start : start+length])
 plt.ylim(-30000, 30000)
 plt.grid(True)
 plt.show()
+
+
+# Split to frames
+frames = list()
+for i in range(0, len(zero_crossings)-1):
+    frames.append(data[zero_crossings[i]:zero_crossings[i+1]])
+print("len(frames):", len(frames))
